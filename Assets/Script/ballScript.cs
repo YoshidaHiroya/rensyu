e84e096e32f3e32b8779abcb183c45db9fa5915e
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-11 10:16:49 +0900

    firstcommit

---
+++ Assets/Script/ballScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ballScript : MonoBehaviour
+{
+	public float speed = 25;
+
+	private void Start()
+	{
+		var force = (transform.right + transform.forward) * speed;
+		this.GetComponent<Rigidbody> ().AddForce (force,ForceMode.VelocityChange);
+	}
+	private void Update(){
+	}
+
+}
---
+++ Assets/Script/paddleScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleScript : MonoBehaviour {
+
+
+		public float accel = 100;
+
+		private void Update()
+		{
+		var force = transform.forward*Input.GetAxisRaw("Horizontal") *accel;
+		GetComponent<Rigidbody> ().AddForce (force,ForceMode.Impulse);
+		}
+	}
+
