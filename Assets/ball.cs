62e71b8660cd23dc2f35e98375d0773b8fe561ab
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-11 17:25:13 +0900

    secondcommit

---
+++ Assets/ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+	public float speed = 25;
+	// Use this for initialization
+	void Start () {
+		var force = new Vector3(0,0,1) * speed;
+		this.GetComponent<Rigidbody> ().AddForce (force,ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/but.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class but : MonoBehaviour {
+	
+		float minAngle = 0.0F;
+		float maxAngle = 90.0F;
+
+		//x.y.zの値を持つVector3型のangleという変数を宣言する
+		public Vector3 angle;
+
+		// Update関数
+		void Update () {
+		if (Input.GetKey("space")) {
+			//angle変数(x,y,z)の数値にTime.deltaTimeを掛けて毎フレームごとに移動するようにする
+			transform.Rotate (angle * Time.deltaTime);
+
+
+		
+		}
+	}
+}
