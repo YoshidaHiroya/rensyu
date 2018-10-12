609bdab27361135f099731d827393fe31e901372
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-12 15:09:14 +0900

    4

--- Assets/Script/ballScript.cs
+++ Assets/Script/ballScript.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public class ballScript : MonoBehaviour
 {
 	public float speed = 25;
+	private GameObject Message;
+	Text text ;
 
 	private void Start()
 	{
+		Message= GameObject.Find ("message");
+		text = Message.GetComponent<Text> ();
 		var force = (transform.right + transform.forward) * speed;
 		this.GetComponent<Rigidbody> ().AddForce (force,ForceMode.VelocityChange);
+
 	}
 	private void Update(){
 	}
 
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "goal1") {
+			text.text="Player2の勝ち！";
+		}
+		if (collision.gameObject.tag == "goal2") {
+
+			text.text="Player1の勝ち！";
+		}
+	}
 }
