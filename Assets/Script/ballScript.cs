fc7f507953dee9ad2e48a81d4a89dfc234aaf41a
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-14 12:02:05 +0900

    5

--- Assets/Script/ballScript.cs
+++ Assets/Script/ballScript.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;//UIをいじるときに必要。Text型を認識しなくなってしまう。
 public class ballScript : MonoBehaviour
 {
 	public float speed = 25;
-	private GameObject Message;
-	Text text ;
+	private GameObject Message;//このスクリプト内でのみ有効なゲームオブジェクトMessageを定義。ここにmessageをいれる。
+	Text text ;//Text型の変数textを定義。
 
 	private void Start()
 	{
-		Message= GameObject.Find ("message");
-		text = Message.GetComponent<Text> ();
+		Message= GameObject.Find ("message");//Hierarchyのmessageオブジェクトを探し出し、さっきのmessageにいれる。
+		text = Message.GetComponent<Text> ();//Messageはmessageを指しているので、TextをGetComponentし、さっきのtextにいれる。
 		var force = (transform.right + transform.forward) * speed;
 		this.GetComponent<Rigidbody> ().AddForce (force,ForceMode.VelocityChange);
 
@@ -19,7 +19,7 @@ public class ballScript : MonoBehaviour
 
 	void OnCollisionEnter(Collision collision){
 		if (collision.gameObject.tag == "goal1") {
-			text.text="Player2の勝ち！";
+			text.text="Player2の勝ち！";//テキスト型変数の中身を変更する。
 		}
 		if (collision.gameObject.tag == "goal2") {
 
---
+++ Assets/Script/dodleScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dodleScript : MonoBehaviour {
+	public float speed =10.0f;
+	public float force =10.0f;
+
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("right")) {
+			rb.AddForce (force,0.0f,0.0f);
+		}
+		if (Input.GetKeyDown("left")) {
+			rb.AddForce (-force,0.0f,0.0f);
+		}
+	}
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag=="yuka"){
+
+			rb.velocity = new Vector3 (0.0f,speed,0.0f);
+		}
+	}
+}
