2f9d1f03cf4651d00a406b52cbae2b9e7beab758
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-14 12:38:23 +0900

    6

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	GameObject sphere;
+	// Use this for initialization
+	void Start () {
+		sphere = GameObject.Find ("Sphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 spherepos = sphere.transform.position;
+		this.transform.position = new Vector3 (this.transform.position.x,spherepos.y,this.transform.position.z);
+	}
+}
--- Assets/Script/dodleScript.cs
+++ Assets/Script/dodleScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class dodleScript : MonoBehaviour {
 	public float speed =10.0f;
 	public float force =10.0f;
-
+	AudioSource audiosource;
 	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody> ();
+		audiosource = this.GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -23,7 +24,7 @@ public class dodleScript : MonoBehaviour {
 	}
 	void OnCollisionEnter(Collision collision){
 		if(collision.gameObject.tag=="yuka"){
-
+			audiosource.Play ();
 			rb.velocity = new Vector3 (0.0f,speed,0.0f);
 		}
 	}
