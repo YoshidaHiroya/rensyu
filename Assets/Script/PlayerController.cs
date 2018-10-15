0dc18f6f374f2a8604dcd2a43f593184e1be6b3c
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-15 23:01:23 +0900

    7

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody rb;
+	Animator animator;
+	public int jumpforce;
+	public int walkforce;
+	int key;
+	float maxwalkspeed;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+		animator = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		if (Input.GetKeyDown ("space")) {
+			this.animator.SetBool ("JumpTrigger",true);
+			rb.AddForce (jumpforce * (this.transform.up));
+
+		}
+		if (Input.GetKeyDown ("right")) {
+			key = 1;
+			rb.AddForce (walkforce * (this.transform.right));
+			transform.localScale=new Vector3 (key,1,1);
+
+			this.animator.SetBool ("JumpTrigger",false);
+		}
+		if (Input.GetKeyDown ("left")) {
+			key = -1;
+			rb.AddForce(-walkforce*(this.transform.right));//Addforce(ベクトル)
+			transform.localScale=new Vector3 (key,1,1);//反転
+
+			this.animator.SetBool ("JumpTrigger",false);
+		}
+		//if((rb.velocity.x)>maxwalkspeed){
+		//	rb.AddForce (key * walkforce * (transform.right));
+		//}
+
+	}
+}
---
+++ Assets/Script/soccorSript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soccorSript : MonoBehaviour {
+	public int speed;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+		rb.velocity = speed*new Vector3(1,1,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag=="yuka"){
+			
+		rb = this.GetComponent<Rigidbody> ();
+		rb.velocity = speed*new Vector3(1,1,0);
+	}
+		if (collision.gameObject.tag == "block") {
+			Destroy (collision.gameObject);
+		}
+
+}
+}
